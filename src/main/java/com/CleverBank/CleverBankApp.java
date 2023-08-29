b90e16a0812755dfff5e7005e2b55bd920475a8b
bende-r <bender0612@mail.ru>
2023-08-29 19:02:09 +0300

    commit

--- src/main/java/com/CleverBank/CleverBankApp.java
+++ src/main/java/com/CleverBank/CleverBankApp.java
@@ -1,28 +1,25 @@
-package CleverBankApp;
+package com.CleverBank;
 
+import DataOperations.Operations;
 import JDBC.SQLFileExecuter;
 import JDBC.SQLFileReader;
 
-import java.awt.*;
 import java.io.*;
 import java.sql.*;
 import java.util.Scanner;
 
+import static java.sql.DriverManager.getConnection;
+
 public class CleverBankApp {
     static final String DB_URL = "jdbc:postgresql://localhost:5432/";
-    static final String USER = "postgres";
-    static final String PASS = "postgres";
+    //static final String USER = "postgres";
+    // static final String PASS = "postgres";
 
     public static void main(String[] argv) {
         SQLFileReader sqlFileReader = new SQLFileReader();
         Scanner in = new Scanner(System.in);
 
         System.out.println("Testing connection to PostgreSQL JDBC");
-
-        Class<?> clazz = CleverBankApp.class;
-        String className = clazz.getName();
-        System.out.println("Полное имя класса: " + className);
-
 
         try {
             Class.forName("org.postgresql.Driver");
@@ -34,8 +31,14 @@ public class CleverBankApp {
 
         System.out.println("PostgreSQL JDBC Driver successfully connected");
 
+        System.out.println("Enter PostrgeSQL Username:");
+        final String USER = in.nextLine();
+
+        System.out.println("Enter password:");
+        final String PASS = in.nextLine();
+
         try {
-            Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            Connection connection = getConnection(DB_URL, USER, PASS);
             Statement statement = connection.createStatement();
 
             System.out.print("Generate a database? Y/N\n");
@@ -61,10 +64,29 @@ public class CleverBankApp {
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-            statement.close();
-            connection.close();
 
             System.out.println("You are connected to the database");
+            while (true) {
+                printMenu();
+                int point = in.nextInt();
+
+                switch (point) {
+                    case 1:
+                        Operations.addingFunds(statement, "4819777576", 100000);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        statement.close();
+                        connection.close();
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Invalid item");
+                }
+            }
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (Exception e) {
@@ -73,4 +95,12 @@ public class CleverBankApp {
 
 
     }
+
+    public static void printMenu() {
+        System.out.println("---------Menu---------");
+        System.out.println("1. Score operations");
+        System.out.println("2. Tables operations");
+        System.out.println("3. Exit");
+    }
+
 }
